e7e7650ed6c1cb51ce485858718559f6eb209e58
StBanas <pio.banasik@gmail.com>
2020-06-10 18:11:45 +0200

    eBayTestingApp - 13.3;

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EbayTestingApp.java
@@ -0,0 +1,21 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EbayTestingApp {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/selenium-drivers/Chrome/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.eBay.com");
+
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("Laptop");
+        inputField.submit();
+    }
+
+
+}
